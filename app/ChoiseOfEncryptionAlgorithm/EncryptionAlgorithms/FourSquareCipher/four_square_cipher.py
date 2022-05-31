0cea08c43fd4f481f4c47576118338273c208874
Chubchik7 <pavel.chub.1997@mail.ru>
2022-05-31 07:58:47 +0300

    creating classes to implement encryption algorithms

---
+++ app/ChoiseOfEncryptionAlgorithm/EncryptionAlgorithms/FourSquareCipher/four_square_cipher.py
@@ -0,0 +1,12 @@
+from CipherOrDecipher.app.Base.BaseCipherOrDecipher import BaseCipherOrDecipher
+
+
+class FourSquareCipher(BaseCipherOrDecipher):
+    def __init__(self, text):
+        super().__init__(text)
+
+    def cipher(self):
+        pass
+
+    def decipher(self):
+        pass
