0da33cd85ba7cf19fbfc0dee9669cfdc7e7664b4
Chubchik7 <pavel.chub.1997@mail.ru>
2022-06-09 08:15:10 +0300

    program code for choise of encryption algorithm

--- app/ChoiseOfEncryptionAlgorithm/choise_of_encryption_algorithm.py
+++ app/ChoiseOfEncryptionAlgorithm/choise_of_encryption_algorithm.py
@@ -1,9 +1,24 @@
-"""
-dictionary = {
-    utils[0..n]: object_class_GUI_utils
-}
-"""
+from PyQt5 import QtWidgets
+from CipherOrDecipher.app.utils.utils import utils
+from CipherOrDecipher.app.ChoiseOfEncryptionAlgorithm.EncryptionAlgorithms.ADFGVXCipher.ADFGVX_cipher \
+    import CipherADFGVXGUI
+from CipherOrDecipher.app.ChoiseOfEncryptionAlgorithm.EncryptionAlgorithms.AtbashMethod.atbash_method \
+    import MethodAtbashGUI
+from CipherOrDecipher.app.ChoiseOfEncryptionAlgorithm.EncryptionAlgorithms.FourSquareCipher.four_square_cipher \
+    import FourSquareCipherGUI
+from CipherOrDecipher.app.ChoiseOfEncryptionAlgorithm.EncryptionAlgorithms.Scrambling.scrambling import ScramblingGUI
+from CipherOrDecipher.app.ChoiseOfEncryptionAlgorithm.EncryptionAlgorithms.SinglePermutationByKey.\
+    single_permutation_by_key import SinglePermutationByKeyGUI
+from CipherOrDecipher.app.ChoiseOfEncryptionAlgorithm.EncryptionAlgorithms.VigenereCipher.vigenere_cipher \
+    import VigenereCipherGUI
+
+CONST_APP = QtWidgets.QApplication([])
 
 algorithm_selection = {
-
+    utils[0]: CipherADFGVXGUI(),
+    utils[1]: MethodAtbashGUI(),
+    utils[2]: FourSquareCipherGUI(),
+    utils[3]: ScramblingGUI(),
+    utils[4]: SinglePermutationByKeyGUI(),
+    utils[5]: VigenereCipherGUI(),
 }
